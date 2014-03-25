d7699f6f395ad5d457aeac6dc980bae184f19ca7
mytoh <mytoh@gmail.com>
2014-03-25 22:54:40 +0900

    update

--- test/run-travis.sh
+++ test/run-travis.sh
@@ -2,23 +2,7 @@
 
 set -e
 
-TEST_PREFIX=/home/travis/build/test
-BUILD_DIR=
-
-build_gauche_release() {
-    SOURCE=Gauche.tgz
-    wget -O $SOURCE http://practical-scheme.net/gauche/latest
-    tar xzf $SOURCE
-    VERSION=`curl -s http://practical-scheme.net/gauche/latest/version`
-    SOURCE_DIR="Gauche-${VERSION}"
-    (cd $SOURCE_DIR; ./configure; make --quiet ; sudo make --quiet install)
-}
-
-build_gauche_head() {
-    SOURCE_DIR="gauche-head"
-    git clone git://github.com/shirok/gauche ${SOURCE_DIR}
-    (cd $SOURCE_DIR; ./DIST gen; ./configure --prefix=${TEST_PREFIX} ; make --quiet ; sudo make --quiet install)
-}
+TEST_PREFIX=/home/travis/build/test_prefix
 
 run_test() {
     ${TEST_PREFIX}/bin/gosh -r7 -I${TRAVIS_BUILD_DIR}/lib ${TRAVIS_BUILD_DIR}/test/test.scm
