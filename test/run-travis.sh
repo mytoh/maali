0879b82a19fa59d5187bc9ec1752df3751dcf464
mytoh <mytoh@gmail.com>
2014-03-25 19:53:34 +0900

    * test/run-travis.sh: update to build git head version

--- test/run-travis.sh
+++ test/run-travis.sh
@@ -2,18 +2,25 @@
 
 set -e
 
-build_gauche() {
+build_gauche_release() {
     SOURCE=Gauche.tgz
     wget -O $SOURCE http://practical-scheme.net/gauche/latest
-    tar xzvf $SOURCE
+    tar xzf $SOURCE
     VERSION=`curl -s http://practical-scheme.net/gauche/latest/version`
     SOURCE_DIR="Gauche-${VERSION}"
     (cd $SOURCE_DIR; ./configure; make; sudo make install)
 }
 
-run_test() {
-    gosh -r7 -I. test.scm
+build_gauche_release() {
+    SOURCE_DIR="gauche-head"
+    git clone git://github.com/shirok/gauche ${SOURCE_DIR}
+    (cd $SOURCE_DIR; ./DIST gen; ./configure --prefix=test ; make; sudo make install)
 }
 
-build_gauche
+run_test() {
+    test/bin/gosh -r7 -I. test.scm
+}
+
+build_gauche_release
+build_gauche_head
 run_test
