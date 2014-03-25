851547d8fe31eb81e39efb9bf64b8f735b0545bc
mytoh <mytoh@gmail.com>
2014-03-25 23:19:27 +0900

    * test/test.scm: fix test

--- test/test.scm
+++ test/test.scm
@@ -11,20 +11,20 @@
 (test-module 'maali) ; モジュールの一貫性チェック
 
 (test-section "test paint noargument")
-(test "feature no colour" "J-_-L" (paint "J-_-L"))
+(test* "feature no colour" "J-_-L" (paint "J-_-L"))
 
 (test-section "test paint one argument")
-(test "feature colour 1" "[33mJ-_-L[0m" (paint "J-_-L" 'yellow))
-(test "feature colour 2" "[38;5;220mJ-_-L[0m" (paint "J-_-L" '(255 200 0)))
-(test "feature colour 3" "[38;5;24mJ-_-L[0m" (paint "J-_-L" "#123456"))
-(test "feature colour 4" "[38;5;24mJ-_-L[0m" (paint "J-_-L" "123456"))
-(test "feature colour 5" "[38;5;255mJ-_-L[0m" (paint "J-_-L" "#fff"))
-(test "feature colour 6" "[38;5;255mJ-_-L[0m" (paint "J-_-L" "fff"))
-(test "feature colour 7" "[38;5;74mJ-_-L[0m" (paint "J-_-L" "#4183C4"))
-(test "feature colour 8" "[38;5;141mJ-_-L[0m" (paint "J-_-L" "MediumPurple"))
+(test* "feature colour 1" "[33mJ-_-L[0m" (paint "J-_-L" 'yellow))
+(test* "feature colour 2" "[38;5;220mJ-_-L[0m" (paint "J-_-L" '(255 200 0)))
+(test* "feature colour 3" "[38;5;24mJ-_-L[0m" (paint "J-_-L" "#123456"))
+(test* "feature colour 4" "[38;5;24mJ-_-L[0m" (paint "J-_-L" "123456"))
+(test* "feature colour 5" "[38;5;255mJ-_-L[0m" (paint "J-_-L" "#fff"))
+(test* "feature colour 6" "[38;5;255mJ-_-L[0m" (paint "J-_-L" "fff"))
+(test* "feature colour 7" "[38;5;74mJ-_-L[0m" (paint "J-_-L" "#4183C4"))
+(test* "feature colour 8" "[38;5;141mJ-_-L[0m" (paint "J-_-L" "MediumPurple"))
 
 (test-section "test unpaint one argument")
-(test "feature unpaint" "test" (unpaint (paint "test" 33)))
+(test* "feature unpaint" "test" (unpaint (paint "test" 33)))
 
 (test-end :exit-on-failure #t)
 
