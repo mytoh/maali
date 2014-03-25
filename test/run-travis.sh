8383424c8461ea74907388d176b94faadb3c1814
mytoh <mytoh@gmail.com>
2014-03-25 19:34:55 +0900

    add function

--- test/run-travis.sh
+++ test/run-travis.sh
@@ -2,11 +2,18 @@
 
 set -e
 
-SOURCE=Gauche.tgz
-wget -O $SOURCE http://practical-scheme.net/gauche/latest
-tar xzvf $SOURCE
-VERSION=`curl -s http://practical-scheme.net/gauche/latest/version`
-SOURCE_DIR="Gauche-${VERSION}"
-(cd $SOURCE_DIR; ./configure; make; sudo make install)
+build_gauche() {
+    SOURCE=Gauche.tgz
+    wget -O $SOURCE http://practical-scheme.net/gauche/latest
+    tar xzvf $SOURCE
+    VERSION=`curl -s http://practical-scheme.net/gauche/latest/version`
+    SOURCE_DIR="Gauche-${VERSION}"
+    (cd $SOURCE_DIR; ./configure; make; sudo make install)
+}
 
-gosh -r7 -I. test.scm
+run_test() {
+    gosh -r7 -I. test.scm
+}
+
+build_gauche
+run_test
