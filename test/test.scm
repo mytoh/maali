9ce389554f5cd96cb8d26f56cd678bc6461f4567
mytoh <mytoh@gmail.com>
2014-03-25 19:27:48 +0900

    add travis-ci files for testing

---
+++ test/test.scm
@@ -0,0 +1,16 @@
+;;; test.scm
+
+(import (scheme base)
+        (scheme write)
+        (gauche base)
+        (maali))
+
+(define (main args)
+  (display "maali loaded"))
+
+
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
